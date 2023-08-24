ebb18904db6ed6c1d3d3c533ff32179d9e67ff10
Joseph Francis <hookedupjoe@users.noreply.github.com>
2023-08-23 21:57:00 -0400

    Support userid retention on refresh

--- appserver/actions/ws-stage.js
+++ appserver/actions/ws-stage.js
@@ -5,6 +5,8 @@ const THIS_MODULE_TITLE = 'Send and receive Websock data related to the stage';
 var isSetup = false;
 var wssMain = false;
 var wsRoom;
+var clients = {};
+var users = {};
 
 module.exports.setup = function setup(scope,options) {
     var config = scope;
@@ -16,22 +18,89 @@ module.exports.setup = function setup(scope,options) {
         this.title = THIS_MODULE_TITLE;
     }
 
+    function getPeopleSummary(){
+        var tmpList = {};
+        for( var aID in users ){
+            var tmpPerson = users[aID];
+            if( tmpPerson.profile && tmpPerson.socketid ){
+                var tmpName = tmpPerson.profile.name || 'Anonymous';
+                var tmpUserID = tmpPerson.userid || 'Anonymous';
+                tmpList[aID] = {name:tmpName, userid: tmpUserID};
+            }
+        }
+        return tmpList;
+    }    
+
+    function resendPeople(){
+        wsRoom.sendDataToAll({action:'people', people: getPeopleSummary()});
+    }
+
+    function updateProfile(theWS, theData){
+        var tmpSocketID = theWS.id;
+        var tmpUserID = theData.userid;
+        var tmpProfile = theData.profile;
+
+        users[tmpUserID] = {
+            socketid: tmpSocketID,
+            userid: tmpUserID,
+            profile: tmpProfile
+        }
+
+        clients[tmpSocketID] = clients[tmpSocketID] || {};
+        clients[tmpSocketID].profile = tmpProfile;
+        clients[tmpSocketID].userid = tmpUserID;
+        resendPeople();
+        //wsRoom.sendDataToAll({action:'people', people: people});
+        console.log('updateProfile',clients);
+    }
     
+    function onConnect(ws){
+        console.log('onConnect',ws.id);
+        ws.userid = $.ws.mgr.getUniqueID();        
+        ws.send(JSON.stringify({action: 'welcome', userid: ws.userid, id: ws.id, people:getPeopleSummary()}))
+    }
+
     function onMessage(ws,data,isBinary){
         console.log('onMessage from websocket room', ws.id, ''+data, isBinary );
+        var tmpData = (''+data).trim();
+        if( tmpData.startsWith('{')){
+            tmpData = JSON.parse(tmpData);
+        }
+        if( tmpData.action ){
+            if( tmpData.action == 'profile' && tmpData.profile){
+                //updateProfile(ws.id,tmpData.profile);
+                updateProfile(ws,tmpData);
+            }
+        }
     }
     function onSocketAdd(theID){
+        //people[theID] = {profile:{name:''}};
         console.log('onSocketAdd from websocket room',theID );
+        //wsRoom.sendDataToAll({action:'people', people: people});
     }
+
     function onSocketRemove(theID){
-        console.log('onSocketRemove from websocket room',theID );
+        if( clients[theID] ){
+            var tmpUserID = clients[theID].userid || '';
+            var tmpUser = users[tmpUserID];
+            if( tmpUser && tmpUser.socketid ){
+                //--- Clear socket it to show not active, but keep user with ID here
+                //     ToDo: cleanup to remove inactive after x period?
+                tmpUser.socketid = '';
+                console.log('cleared socketid',tmpUserID);
+            }
+            delete clients[theID];
+            resendPeople();
+            //wsRoom.sendDataToAll({action:'people', people: people});
+        }
+        console.log('onSocketRemove from websocket room',theID, clients);
     }
 
     if( options.websocket === true ){
 
         if( !isSetup ){
             wssMain = new $.ws.WebSocketServer({ noServer: true });
-            wsRoom = new $.ws.WebSocketRoom({name:'stage', server: wssMain, onMessage: onMessage, onSocketAdd: onSocketAdd, onSocketRemove: onSocketRemove});
+            wsRoom = new $.ws.WebSocketRoom({name:'stage', server: wssMain, onConnect: onConnect, onMessage: onMessage, onSocketAdd: onSocketAdd, onSocketRemove: onSocketRemove});
 
             isSetup = true;
             console.log('The stage has created new websock room')
